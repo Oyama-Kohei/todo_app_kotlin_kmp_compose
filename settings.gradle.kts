3c3964b0cbf716231dda09d7c2b7c1d5dc985293
OyamaKohei <kohei.perc.sd@gmail.com>
2024-12-04 17:10:42 +0900

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "todo_app_kotlin_kmp_compose"
+include(":androidApp")
+include(":shared")
